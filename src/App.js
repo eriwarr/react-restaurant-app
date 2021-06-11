80d7cad49a943155aee8fe13872d005904b8a54e
Eric Warren II <ericwarrenii@Erics-MacBook-Pro.local>
2021-06-11 15:21:35 -0400

    added toggle function to button'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { Component } from 'react';
-import Modal from './Modal';
 const buffaloChicken = "https://blogchef.net/wp-content/uploads/2009/01/buffalo_chicken_pizza_1-1.jpg";
 const calzone = "https://www.happyfoodstube.com/wp-content/uploads/2016/03/calzone-pizza-recipe.jpg";
 const wings= "https://cdn.vox-cdn.com/thumbor/37Er_wqoqG0DFAwK3k8t8-xv7bw=/0x0:1440x1440/1200x900/filters:focal(605x605:835x835)/cdn.vox-cdn.com/uploads/chorus_image/image/61877527/118316077_1579861602185270_1546917259458022944_o.19.jpg";
@@ -29,6 +28,7 @@ class Order extends Component {
     super(props);
     this.state = {
       order: [],
+      show: false,
     }
     this.subtotal = this.subtotal.bind(this);
   }
@@ -44,10 +44,19 @@ class Order extends Component {
        order.Value
     ));
 
+    const orderItems = this.props.order.map((item, index) => (
+      <li>
+        <h2>{item.Food}</h2>
+        <p>{item.Price}</p>
+      </li>
+    ))
+
     return (
       <>
-      <div>Cart ({total.length})</div>
-      <div>Subtotal: ${this.subtotal(total)}</div>
+        <div>Cart ({total.length})</div>
+        <div>Subtotal: ${this.subtotal(total)}</div>
+        <button onClick={() => this.setState({show: !this.state.show})}>{this.state.show ? 'Hide order' : 'Show order'}</button>
+        { this.state.show && <ul>{orderItems}</ul> }
       </>
     )
   }
@@ -97,8 +106,6 @@ class Menu extends Component {
         </div>
         <aside className="order">
           <Order order={this.state.order} />
-          <button>View Cart</button>
-          <Modal/>
         </aside>
       </div>
       </>
